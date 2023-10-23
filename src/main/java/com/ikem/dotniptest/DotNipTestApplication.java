920a56bc2e57e3a21f3c9abb399deaf2b8223d0d
Ikemefuna <nwodofrank@gmail.com>
2023-10-23 15:44:06 +0100

    setup

---
+++ src/main/java/com/ikem/dotniptest/DotNipTestApplication.java
@@ -0,0 +1,13 @@
+package com.ikem.dotniptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotNipTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotNipTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ikem/dotniptest/DotNipTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikem.dotniptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotNipTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
